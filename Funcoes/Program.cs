8d45f03b35ddd5f3edfd8925b65ba85216e60b29
Leonardo <leoccosta@outlook.com>
2020-12-05 20:25:46 -0300

    Adiciona projeto Funcoes

---
+++ Funcoes/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Funcoes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] valores = { 1, 8, 3, 6, 2, 5, 9, 13, 0, 2 };
+            int valorMaximo = ValorMaximo(valores);
+
+            Console.WriteLine($"O valor máximo do array é: { valorMaximo }");
+
+            int[] valores2 = { 0, 2, 3, 4, 9, 25, 89, 4, 100 };
+            int valorMaximo2 = ValorMaximo(valores2);
+
+            Console.WriteLine($"O 2o. valor máximo do array é: { valorMaximo2 }");
+        }
+
+        static int ValorMaximo(int[] numeros)
+        {
+            int valorMaximo = 0;
+            for (int i = 0; i < numeros.Length; i++)
+            {
+                if (numeros[i] > valorMaximo)
+                {
+                    valorMaximo = numeros[i];
+                }
+            }
+
+            return valorMaximo;
+        }
+    }
+}
--- PrimeiroPrograma/Program.cs
+++ PrimeiroPrograma/Program.cs
@@ -10,10 +10,8 @@ namespace PrimeiroPrograma
         }
 
         static void VariaveisETipos() {
-                        // declaração com tipo explícito
+            // declaração com tipo explícito
             string nome;
-
-            // *** explicar que todo fim de uma instrução deve terminar com ;
 
             string @namespace = "Algum valor";
             double _valorTotal = 200;
@@ -29,10 +27,6 @@ namespace PrimeiroPrograma
             var numeroDecimal = 200M;
             var numeroFloat = 200F;
 
-            // *** camel case
-
-            // *** explicar comentário em linha e em bloco
-
             // não permitidos
             /*
             int 123var = 1;
@@ -41,70 +35,69 @@ namespace PrimeiroPrograma
             string static  = 100;
             */
 
+            /* Tipos */
+            
+            // bool
 
-        	/* Tipos */
-        	
-        	// bool
+            bool souProgramador = true;
+            Console.WriteLine("Sou programador? " + souProgramador);
 
-        	bool souProgramador = true;
-        	Console.WriteLine("Sou programador? " + souProgramador);
+            // a diferença é a capacidade de armazenamento
 
-        	// a diferença é a capacidade de armazenamento
+            // byte 
 
-    		// byte 
+            byte idade = 30;
+            
+            Console.WriteLine("MinValue: " + byte.MinValue);
+            Console.WriteLine("MaxValue: " + byte.MaxValue);
 
-        	byte idade = 30;
-        	
-        	Console.WriteLine("MinValue: " + byte.MinValue);
-        	Console.WriteLine("MaxValue: " + byte.MaxValue);
+            sbyte temperatura = -7;
 
-        	sbyte temperatura = -7;
+            Console.WriteLine("MinValue: " + sbyte.MinValue);
+            Console.WriteLine("MaxValue: " + sbyte.MaxValue);
 
-        	Console.WriteLine("MinValue: " + sbyte.MinValue);
-        	Console.WriteLine("MaxValue: " + sbyte.MaxValue);
+            // short
 
-        	// short
+            short pontos = 100;
+            
+            Console.WriteLine("MinValue: " + short.MinValue);
+            Console.WriteLine("MaxValue: " + short.MaxValue);
 
-        	short pontos = 100;
-        	
-        	Console.WriteLine("MinValue: " + short.MinValue);
-        	Console.WriteLine("MaxValue: " + short.MaxValue);
+            // int
 
-        	// int
+            int distancia = 5000;
+            
+            Console.WriteLine("MinValue: " + int.MinValue);
+            Console.WriteLine("MaxValue: " + int.MaxValue);
 
-        	int distancia = 5000;
-        	
-        	Console.WriteLine("MinValue: " + int.MinValue);
-        	Console.WriteLine("MaxValue: " + int.MaxValue);
+            // ulong
 
-        	// ulong
+            ulong habitantes = 8_000_000_000;
+            
+            Console.WriteLine("MinValue: " + ulong.MinValue);
+            Console.WriteLine("MaxValue: " + ulong.MaxValue);
 
-        	ulong habitantes = 8_000_000_000;
-        	
-        	Console.WriteLine("MinValue: " + ulong.MinValue);
-        	Console.WriteLine("MaxValue: " + ulong.MaxValue);
+            // float
 
-        	// float
+            float preco = 23.99F;
+            
+            Console.WriteLine("MinValue: " + float.MinValue);
+            Console.WriteLine("MaxValue: " + float.MaxValue);
 
-        	float preco = 23.99F;
-        	
-        	Console.WriteLine("MinValue: " + float.MinValue);
-        	Console.WriteLine("MaxValue: " + float.MaxValue);
+               // double
 
-   	        // double
-
-        	double totalDoPedido = 23.99;
-        	
-        	Console.WriteLine("MinValue: " + double.MinValue);
-        	Console.WriteLine("MaxValue: " + double.MaxValue);
+            double totalDoPedido = 23.99;
+            
+            Console.WriteLine("MinValue: " + double.MinValue);
+            Console.WriteLine("MaxValue: " + double.MaxValue);
 
             // decimal
 
-        	decimal distanciaPlanetaria = 78_999_999_999_999_999_000_149_600_000.00m;
+            decimal distanciaPlanetaria = 78_999_999_999_999_999_000_149_600_000.00m;
             decimal medidaSubatomico = 0.0000000000000000000000000000000000000000000000000000000000000000000000000000001m;
-        	
-        	Console.WriteLine("MinValue: " + decimal.MinValue);
-        	Console.WriteLine("MaxValue: " + decimal.MaxValue);
+            
+            Console.WriteLine("MinValue: " + decimal.MinValue);
+            Console.WriteLine("MaxValue: " + decimal.MaxValue);
 
             /* Expressões */
 
@@ -137,8 +130,6 @@ namespace PrimeiroPrograma
             int resultado =  6 * 10 / (valor1 + valor2);
             Console.WriteLine("O resultado da expressão é: " + resultado);
 
-             // *** explicar precedência
-
             // Operadores matemáticos (unários)
 
             resultado = +valor1;
@@ -151,7 +142,7 @@ namespace PrimeiroPrograma
             // Operadores de atribuição
 
             int total = 10;
-        	int valor = 5;
+            int valor = 5;
 
             total += valor;
             total -= valor;
